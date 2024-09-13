734e94f3a2eb04b4fccc33e08dae216ebb24f410
curly328x2 <curly328x2@gmail.com>
2024-09-13 23:48:11 +0900

    first commit

---
+++ src/main/java/com/tool/TiffCreater/TiffCreaterApplication.java
@@ -0,0 +1,13 @@
+package com.tool.TiffCreater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiffCreaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiffCreaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tool/TiffCreater/TiffCreaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.TiffCreater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiffCreaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
