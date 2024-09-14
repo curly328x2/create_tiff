4fa79b26e1f91795585fb94e8613a3f452a86a46
curly328x2 <curly328x2@gmail.com>
2024-09-14 22:03:26 +0900

    実行クラス修正

--- src/main/java/com/tool/TiffCreater/TiffCreaterApplication.java
+++ src/main/java/com/tool/TiffCreater/TiffCreaterApplication.java
@@ -1,13 +1,28 @@
 package com.tool.TiffCreater;
 
+import com.tool.TiffCreater.service.CreateTiffService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
-public class TiffCreaterApplication {
+public class TiffCreaterApplication implements CommandLineRunner {
+
+	private final CreateTiffService createTiffService;
+
+	public TiffCreaterApplication(CreateTiffService createTiffService) {
+		this.createTiffService = createTiffService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiffCreaterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws IOException {
+		createTiffService.convertPdfToTiff();
 	}
 
 }
